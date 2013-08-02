439836006e66f26865fac0af1f0cda078ea7c2f2
Amitej <amitej.kalra@intuzion.com>
2013-08-02 12:50:42 +0530

    pusher

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,6 @@ Privatechat::Application.initialize!
   Pusher.key    = 'aaf22ec58ddbff4643d5'
   Pusher.secret = '04673d7bb7f205cf86b9'
 
-  PUSHER_JS_DOMAIN = 'http://js.pusher.com'
-PUSHER_SOCKET_HOST = 'ws.pusherapp.com'
-PUSHER_WS_PORT = 80
+#   PUSHER_JS_DOMAIN = 'http://js.pusher.com'
+# PUSHER_SOCKET_HOST = 'ws.pusherapp.com'
+# PUSHER_WS_PORT = 80
